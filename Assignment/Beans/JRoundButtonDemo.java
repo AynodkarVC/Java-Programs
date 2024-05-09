77912bd708726213d0c436f4c1eedb11287b7b5e
Vishal Aynodkar <aynodkar@hotmail.com>
2024-05-09 11:22:03 +0530

    JRoundButton

---
+++ Assignment/Beans/JRoundButtonDemo.java
@@ -0,0 +1,32 @@
+package Beans;
+
+import javax.swing.*;
+
+public class JRoundButtonDemo extends JFrame{
+    JRoundButton j1,j2;
+    JTextField t;
+
+    public JRoundButtonDemo(){
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+
+        j1 = new JRoundButton("Click");
+        j1.setBounds(10,10,56,56);
+        add(j1);
+
+        t = new JTextField();
+        t.setBounds(80,10,56,56);
+        add(t);
+
+        j2 = new JRoundButton("Clear");
+        j2.setBounds(176,10,56,56);
+        add(j2);
+
+        setVisible(true);
+    }
+    public static void main(String args[]){
+        new JRoundButtonDemo();
+    }
+}
